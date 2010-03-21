5be7f14a834da1903de4e69ea27b51da8ed028f1
Thomas Leonard <talex5@gmail.com>
2010-03-21 15:28:18 +0000

    Record retired feeds
    These are feeds that we used to mirror, but we have stopped checking for
    updated because the upstream feed has disappeared.

--- stats.py
+++ stats.py
@@ -21,6 +21,7 @@ aliases = {
 		'1DFE86921CBA7BCB691DA2434F5A1693E18E1E91' : '0C5C7BC77B70E7BA813478B6FF29FF60ACB8DFE8',
 		'2E2B4E59CAC8D874CD2759D34B1095AF2E992B19' : 'C82D382AAB381A54529019D6A0F9B035686C6996',
 		'DA9825AECAD089757CDABD8E07133F96CA74D8BA' : '92429807C9853C0744A68B9AAE07828059A53CC1',
+		'7722DC5085B903FF176CCAA9695BA303C9839ABC' : '03DC5771716A5A329CA97EA64AB8A8E7613A266F',
 }
 
 # Feeds with these keys must not be mirrored
@@ -28,8 +29,9 @@ test_keys = set()
 test_keys.add('5E22F6A13A76F396AC68B5F29B1F5D7F9721DA90')
 test_keys.add('2E32123D8BE241A3B6D91E0301685F11607BB2C5')
 
-def make_feed_element(parent, feed):
+def make_feed_element(parent, feed, active):
 	feed_element = ET.SubElement(parent, 'feed')
+	feed_element.attrib['active'] = str(active)
 	feed_element.attrib['local-dir'] = get_feed_dir(feed.url).replace('#', '%23')
 	feed_element.attrib['url'] = feed.url
 	feed_element.attrib['name'] = feed.get_name()
@@ -44,7 +46,7 @@ def contents(path):
 		return stream.read()
 
 def write_if_changed(xml, path):
-	new = path + '.net'
+	new = path + '.new'
 	xml.write(new, encoding='utf-8')
 	if contents(path) == contents(new):
 		os.unlink(new)
@@ -75,20 +77,24 @@ def count_impls(url):
 
 class User:
 	def __init__(self):
-		self.feeds = set()
+		self.feeds = {}
 		self.last_active = None
 		self.n_feeds = 0
 		self.n_implementations = 0
+		self.n_inactive = 0
 		self.key = None
 	
-	def add_feed(self, feed, sig):
+	def add_feed(self, feed, sig, active):
 		assert feed not in self.feeds, feed
-		self.feeds.add(feed)
+		self.feeds[feed] = active
 		mtime = sig.get_timestamp()
 		if self.last_active is None or self.last_active < mtime:
 			self.last_active = mtime
-		self.n_feeds += 1
-		self.n_implementations += count_impls(feed.url)
+		if active:
+			self.n_feeds += 1
+			self.n_implementations += count_impls(feed.url)
+		else:
+			self.n_inactive += 1
 	
 	def as_xml(self):
 		root = ET.Element('user')
@@ -99,19 +105,21 @@ class User:
 
 		feeds = ET.SubElement(root, 'feeds')
 
-		sorted_feeds = sorted([(feed.get_name().lower(), feed) for feed in self.feeds])
+		sorted_feeds = sorted([(feed.get_name().lower(), feed) for feed in self.feeds.keys()])
 		for unused, feed in sorted_feeds:
-			make_feed_element(feeds, feed)
+			make_feed_element(feeds, feed, self.feeds[feed])
 
 		stats = ET.SubElement(root, 'stats')
 		stats.attrib['feeds'] = str(self.n_feeds)
 		stats.attrib['implementations'] = str(self.n_implementations)
+		if self.n_inactive:
+			stats.attrib['inactive_feeds'] = str(self.n_inactive)
 		stats.attrib['karma'] = str(self.get_karma())
 
 		return ET.ElementTree(root)
 
 	def get_karma(self):
-		return 10 * self.n_feeds + self.n_implementations
+		return 10 * self.n_feeds + self.n_implementations + self.n_inactive
 
 def export_users(pairs):
 	root = ET.Element('users')
@@ -137,23 +145,32 @@ class Stats:
 		self.users = {}		# Fingerprint -> User
 		self.sites = {}		# Domain -> [Feed]
 		self.feeds = []
+		self.active = {}	# Feed -> bool
 	
-	def add_feed(self, feed):
+	def add_feed(self, feed, active):
+		self.active[feed] = active
+
 		metadata = ET.Element('metadata')
+		metadata.attrib["active"] = str(active)
 
 		sigs = iface_cache.get_cached_signatures(feed.url)
 
-		for sig in sigs:
-			fingerprint = aliases.get(sig.fingerprint, sig.fingerprint)
-			assert fingerprint not in aliases, fingerprint
-			assert fingerprint not in test_keys, (fingerprint, feed)
-			if fingerprint not in self.users:
-				self.users[fingerprint] = User()
-			self.users[fingerprint].add_feed(feed, sig)
+		for sig in sigs or []:
+			if isinstance(sig, gpg.ValidSig):
+				fingerprint = aliases.get(sig.fingerprint, sig.fingerprint)
+				assert fingerprint not in aliases, fingerprint
+				if active:
+					assert fingerprint not in test_keys, (fingerprint, feed)
+				if fingerprint not in self.users:
+					self.users[fingerprint] = User()
+				self.users[fingerprint].add_feed(feed, sig, active)
 
-			signer = ET.SubElement(metadata, "signer")
-			signer.attrib["user"] = fingerprint
-			signer.attrib["date"] = format_date(sig.get_timestamp())
+				signer = ET.SubElement(metadata, "signer")
+				signer.attrib["user"] = fingerprint
+				signer.attrib["date"] = format_date(sig.get_timestamp())
+			else:
+				signer = ET.SubElement(metadata, "signer")
+				signer.attrib["error"] = str(sig)
 
 		domain = trust.domain_from_url(feed.url)
 		if domain not in self.sites:
@@ -190,7 +207,7 @@ class Stats:
 			feeds_elem = ET.SubElement(site, "feeds")
 			sorted_feeds = sorted([(feed.get_name().lower(), feed) for feed in feeds])
 			for name, feed in sorted_feeds:
-				make_feed_element(feeds_elem, feed)
+				make_feed_element(feeds_elem, feed, self.active[feed])
 			site_xml = ET.ElementTree(site)
 			write_if_changed(site_xml, os.path.join(topdir, 'sites', 'site-%s.xml' % domain))
 
@@ -200,9 +217,10 @@ class Stats:
 
 		for feed, metadata in self.feeds:
 			for signer in metadata.findall("signer"):
-				fingerprint = signer.attrib["user"]
-				user = self.users[fingerprint]
-				signer.attrib["name"] = user.key.get_short_name()
+				if "user" in signer.attrib:
+					fingerprint = signer.attrib["user"]
+					user = self.users[fingerprint]
+					signer.attrib["name"] = user.key.get_short_name()
 
 			metadata_xml = ET.ElementTree(metadata)
 			feed_dir = get_feed_dir(feed.url)
